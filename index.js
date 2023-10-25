b5f33eba70cc8ccabb95cccdaa80f1d25dfef8ef
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-10-25 18:52:08 -0500

    working on logic of indexjs

--- index.js
+++ index.js
@@ -1,71 +1,82 @@
 // TODO: Include packages needed for this application
-// to writefile package
 const fs = require('fs');
+const inquirer = require('inquirer');
 const { generateMarkdown } = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [{
-  type: 'input',
-  message: 'What is your project title?',
-  name: 'projectName'
-},
-{
-  type: 'input',
-  message: 'What is the description of your project?',
-  name: 'description'
-},
-{
-  type: 'input',
-  message: 'What is your installation instructions?',
-  name: 'installation'
-},
-{
-  type: 'input',
-  message: 'What is your usage information?',
-  name: 'usage'
-},
-{
-  type: 'input',
-  message: 'What is your contribution guidelines?',
-  name: 'contribution'
-},
-{
-  type: 'input',
-  message: 'What is your test instructions?',
-  name: 'test'
-},
-{
-  type: 'list',
-  message: 'What is your license?',
-  name: 'license',
-  choices: ["Boost Software License 1.0",
-    "MIT license",
-    "Apache License",
-    "The Unlicense",
-    "GNU General Public License v3.0"]
-},
-{
-  type: 'input',
-  message: 'What is your email address?',
-  name: 'email'
-},
-{
-  type: 'input',
-  message: `What is your Github username?`,
-  name: 'github'
-}];
+const questions = [
+  {
+    type: 'input',
+    name: 'projectName',
+    message: 'What is your project title?',
+  },
+  {
+    type: 'input',
+    message: 'What is the description of your project?',
+    name: 'description'
+  },
+  {
+    type: 'input',
+    message: 'What is your installation instructions?',
+    name: 'installation'
+  },
+  {
+    type: 'input',
+    message: 'What is your usage information?',
+    name: 'usage'
+  },
+  {
+    type: 'input',
+    message: 'What is your contribution guidelines?',
+    name: 'contribution'
+  },
+  {
+    type: 'input',
+    message: 'What is your test instructions?',
+    name: 'test'
+  },
+  {
+    type: 'list',
+    message: 'What is your license?',
+    name: 'license',
+    choices: ["Boost Software License 1.0",
+      "MIT license",
+      "Apache License",
+      "The Unlicense",
+      "GNU General Public License v3.0"]
+  },
+  {
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email'
+  },
+  {
+    type: 'input',
+    message: `What is your Github username?`,
+    name: 'github'
+  }]
+
+//store inquirer prompt to get answers in function prompt
+function prompt() {
+  return inquirer.prompt(questions).then(answers => {
+    console.log(answers);
+    return answers;
+  })
+}
 
 // TODO: Create a function to write README file
-function writeToFile(questions) {
-  fs.writeFile('README.md', questions, (err) =>
+function writeToFile(answers) {
+  fs.writeFile('README.md', answers, (err) =>
     err ? console.error(err) : console.log('Done!'))
 }
 
 // TODO: Create a function to initialize app
 function init() {
-  generateMarkdown()
-  writeToFile()
+  prompt(questions).then(answers => {
+    writeToFile(answers)
+  })
+  generateMarkdown(answers)
 }
 
 // Function call to initialize app
-init(questions);
+init();
